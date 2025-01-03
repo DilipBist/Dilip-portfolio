a704b22f92c23af4e9b3db0830ead70e68f40fb0
Dilip-Bist <dilipbist879@gmail.com>
2025-01-03 20:32:02 +0545

    skill added

--- data.js
+++ data.js
@@ -2,12 +2,12 @@ export const skills = [
     { name: 'Figma', image: './image/figma.png', percentage: '70%' },
     { name: 'HTML', image: './image/html.png', percentage: '95%' },
     { name: 'CSS', image: './image/css.png', percentage: '90%' },
-    { name: 'SASS', image: './image/sass.png', percentage: '50%' },
+    { name: 'SASS', image: './image/sass.png', percentage: '90%' },
     { name: 'Bootstrap', image: './image/bootstrap.png', percentage: '60%' },
-    { name: 'Tailwind', image: './image/tailwind.png', percentage: '60%' },
+    { name: 'Tailwind', image: './image/tailwind.png', percentage: '70%' },
     { name: 'JavaScript', image: './image/js.png', percentage: '60%' },
-    { name: 'jQuery', image: './image/jquery.png', percentage: '80%' },
-    { name: 'React', image: './image/react.png', percentage: '60%' }
+    { name: 'jQuery', image: './image/jquery.png', percentage: '60%' },
+    { name: 'React', image: './image/react.png', percentage: '70%' }
 ];
 
 export function renderSkills(skills) {
